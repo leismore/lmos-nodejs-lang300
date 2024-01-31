8d073161514614291d511b4cc9ea66ab0b51d57b
Kyle Chine <kyle.chine@leismore.co>
2024-01-31 19:48:29 +1100

    get_handler2 done

---
+++ src/lib/get_handler2.ts
@@ -0,0 +1,35 @@
+/**
+ * Checking HTTP Accept-Language
+ */
+
+import { Request, Response, NextFunction } from 'express';
+import resolveAcceptLanguage from 'resolve-accept-language';
+import CONFIG from '../config.json' assert {type: 'json'};
+import { lowercase_mapping } from './lowercase_mapping.js';
+
+const mapping       = lowercase_mapping();
+const langSupported = Object.keys(mapping);
+const langDefault   = CONFIG.defaultLang.toLowerCase();
+
+function get_handler2(req:Request, res:Response, next:NextFunction):void
+{
+    let lang:(string|null) = res.locals.lang;
+    let langRequest:string;
+
+    if (lang === null)
+    {
+        if ('accept-language' in req.headers && typeof req.headers['accept-language'] === 'string')
+        {
+            langRequest = req.headers['accept-language'];
+            res.locals.lang = resolveAcceptLanguage.default(langRequest, langSupported, langDefault);
+        }
+        else
+        {
+            res.locals.lang = langDefault;
+        }
+    }
+    
+    next();
+}
+
+export { get_handler2 };
