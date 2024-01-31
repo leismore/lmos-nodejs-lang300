2ed07cc6be73cf26a94a6d98c2402dc9a252434c
Kyle Chine <kyle.chine@leismore.co>
2024-01-31 20:37:58 +1100

    get_handler3 done

---
+++ src/lib/get_handler3.ts
@@ -0,0 +1,27 @@
+/**
+ * Redirection
+ */
+
+import { Request, Response } from 'express';
+import { lowercase_mapping } from './lowercase_mapping.js';
+import { LMResponse, LMResponseData } from '@leismore/lmos-nodejs-lmresponse';
+import CONFIG from '../config.json' assert {type: 'json'};
+
+const mapping     = lowercase_mapping();
+const statusCode  = CONFIG.redirection.statusCode;
+
+function get_handler3(req:Request, res:Response):void
+{
+    const resp        = new LMResponse(res);
+    const lang:string = res.locals.lang;
+    const url         = mapping[lang];
+
+    const respData:LMResponseData = {
+        statusCode: statusCode,
+        headers: {Location: url}
+    }
+
+    resp.send(respData);
+}
+
+export { get_handler3 };
