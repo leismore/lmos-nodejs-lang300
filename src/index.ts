6d4544a02772d3f002b2ddd10d4f9618ae23e393
Kyle Chine <kyle.chine@leismore.co>
2024-01-31 20:52:34 +1100

    change UK to GB

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,22 @@
  * res.locals:
  *   lang:(string|null) - The selected language tag (lowercase)
  */
+
+import express from 'express';
+import CONFIG from './config.json' assert {type: 'json'};
+import PACKAGE from '../package.json' assert {type: 'json'};
+import { get_handler1 } from './lib/get_handler1.js';
+import { get_handler2 } from './lib/get_handler2.js';
+import { get_handler3 } from './lib/get_handler3.js';
+
+const appName = PACKAGE.name;
+const port = CONFIG.app.port;
+
+const app = express();
+
+
+app.get('/', get_handler1, get_handler2, get_handler3);
+
+app.listen(port, CONFIG.app.host, CONFIG.app.backlog, () => {
+  console.log(`${appName} listening on port ${port}`)
+})
