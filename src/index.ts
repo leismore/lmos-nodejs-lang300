98f89302b6e01d227d111db7c8fcb25d7c7f0c5d
Kyle Chine <kyle.chine@leismore.co>
2024-01-31 18:52:14 +1100

    get_handler1 done

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/**
+ * res.locals:
+ *   lang:(string|null) - The selected language tag
+ */
---
+++ src/lib/get_handler1.ts
@@ -0,0 +1,31 @@
+/**
+ * Checking the language-tag cookie
+ */
+
+import { Request, Response, NextFunction } from 'express';
+import CONFIG from '../config.json' assert {type: 'json'};
+import { lowercase_mapping } from './lowercase_mapping.js';
+
+const cookieName    = CONFIG.cookies.langTag;
+const mapping       = lowercase_mapping();
+const langSupported = Object.keys(mapping);
+
+function get_handler1(req:Request, res:Response, next:NextFunction):void
+{
+    res.locals.lang = null;
+    let cookieValue:string;
+
+    if (cookieName in req.cookies && typeof req.cookies[cookieName] === 'string')
+    {
+        cookieValue = req.cookies[cookieName].toLowerCase();
+
+        if (cookieValue in langSupported)
+        {
+            res.locals.lang = cookieValue;
+        }
+    }
+
+    next();
+}
+
+export { get_handler1 };
