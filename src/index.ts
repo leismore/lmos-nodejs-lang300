50494421eacfa2ee01b20dd2aced6afaa8c09732
Kyle Chine <kyle.chine@leismore.co>
2024-02-01 11:41:40 +1100

    update config.json for port number should be string

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
  */
 
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import CONFIG from './config.json' assert {type: 'json'};
 import { get_handler1 } from './lib/get_handler1.js';
 import { get_handler2 } from './lib/get_handler2.js';
@@ -13,7 +14,7 @@ const appName = CONFIG.app.name;
 const port = CONFIG.app.port;
 
 const app = express();
-
+app.use(cookieParser());
 
 app.get('/', get_handler1, get_handler2, get_handler3);
 
