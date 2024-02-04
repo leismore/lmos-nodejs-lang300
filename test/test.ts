73e59bb114a304a89ba19bfd4b26f883810ca2c9
Kyle Chine <kyle.chine@leismore.co>
2024-02-04 17:29:08 +1100

    test finished

--- test/test.ts
+++ test/test.ts
@@ -35,7 +35,7 @@ const requestHeaders_cookie2:{[key:string]:string} = {
   'Cookie': `${cookieLangTag}=en-US`
 };
 
-// 5. With a unsupported cookie; Should be en-GB
+// 5. With an unsupported cookie; Should be en-GB
 const requestHeaders_cookie3:{[key:string]:string} = {
   'Accept-Language': 'en-US;q=0.9, en-GB, en;q=0.8, zh-CN;q=0.7, zh;q=0.6',
   'Cookie': `${cookieLangTag}=zh-CN`
@@ -155,5 +155,141 @@ describe(`${appName} - Unit Test`, function(){
     });
 
   }); // it #3
+
+  it('4. Testing with Cookies - Should return en-US', function(){
+
+    return axios.get(
+      endpoint,
+      {
+        headers:         requestHeaders_cookie2,
+        maxRedirects:    0,
+        validateStatus:  (status:number):boolean => {
+          if (status === Number(statusCode))
+          {
+            return true;
+          }
+          else
+          {
+            return false;
+          }
+        }
+      }
+    )
+    .then( res => {
+      assert
+      (
+        ( res.status                        === Number(statusCode) &&
+          res.headers['location']           === mapping['en-us']   &&
+          res.headers['vary'].toLowerCase() === 'accept-language'
+        ),
+        ( '\n' + `Status Code: ${res.status}`           + '\n' +
+                 `Location:    ${res.headers.location}` + '\n' +
+                 `Vary:        ${res.headers.vary}` )
+      );
+    });
+
+  }); // it #4
+
+  it('5. Testing with an Unsupported Cookie - Should return en-GB', function(){
+
+    return axios.get(
+      endpoint,
+      {
+        headers:         requestHeaders_cookie3,
+        maxRedirects:    0,
+        validateStatus:  (status:number):boolean => {
+          if (status === Number(statusCode))
+          {
+            return true;
+          }
+          else
+          {
+            return false;
+          }
+        }
+      }
+    )
+    .then( res => {
+      assert
+      (
+        ( res.status                        === Number(statusCode) &&
+          res.headers['location']           === mapping['en-gb']   &&
+          res.headers['vary'].toLowerCase() === 'accept-language'
+        ),
+        ( '\n' + `Status Code: ${res.status}`           + '\n' +
+                 `Location:    ${res.headers.location}` + '\n' +
+                 `Vary:        ${res.headers.vary}` )
+      );
+    });
+
+  }); // it #5
+
+  it('6. Testing Unsupported Accept-Language, without Cookies - Should return the default language', function(){
+
+    return axios.get(
+      endpoint,
+      {
+        headers:         requestHeaders_default1,
+        maxRedirects:    0,
+        validateStatus:  (status:number):boolean => {
+          if (status === Number(statusCode))
+          {
+            return true;
+          }
+          else
+          {
+            return false;
+          }
+        }
+      }
+    )
+    .then( res => {
+      assert
+      (
+        ( res.status                        === Number(statusCode)   &&
+          res.headers['location']           === mapping[defaultLang] &&
+          res.headers['vary'].toLowerCase() === 'accept-language'
+        ),
+        ( '\n' + `Status Code: ${res.status}`           + '\n' +
+                 `Location:    ${res.headers.location}` + '\n' +
+                 `Vary:        ${res.headers.vary}` )
+      );
+    });
+
+  }); // it #6
+
+  it('7. Testing with an Unsupported Cookie and an Unsupported Accept-Language - Should return the default language', function(){
+
+    return axios.get(
+      endpoint,
+      {
+        headers:         requestHeaders_default2,
+        maxRedirects:    0,
+        validateStatus:  (status:number):boolean => {
+          if (status === Number(statusCode))
+          {
+            return true;
+          }
+          else
+          {
+            return false;
+          }
+        }
+      }
+    )
+    .then( res => {
+      assert
+      (
+        ( res.status                        === Number(statusCode)   &&
+          res.headers['location']           === mapping[defaultLang] &&
+          res.headers['vary'].toLowerCase() === 'accept-language'
+        ),
+        ( '\n' + `Status Code: ${res.status}`           + '\n' +
+                 `Location:    ${res.headers.location}` + '\n' +
+                 `Vary:        ${res.headers.vary}` )
+      );
+    });
+
+  }); // it #7
 
 }); // Describe
